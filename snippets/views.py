75dbac2dc9ca2237a0860ac2e6d4f9c86695183f
fey101 <faithkangai.g@gmail.com>
2015-05-23 14:26:45 +0300

    final commit
    everything is now set for a simple pastebin browsable api

--- snippets/views.py
+++ snippets/views.py
@@ -4,46 +4,36 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions, renderers
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import viewsets
 
-class SnippetList(generics.ListCreateAPIView):
+
+#class SnippetViewSet replaces the SnippetList, SnippetDetail and SnippetHighlight view classes
+class SnippetViewSet(viewsets.ModelViewSet):
+	"""
+	This viewset automatically provides `list`, `create`, `retrieve`,
+	`update` and `destroy` actions.
+
+	Additionally we also provide an extra `highlight` action.
+	"""
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+	@detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+	def highlight(self, request, *args, **kwargs):
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
 
 	def perform_create(self, serializer):
 		serializer.save(owner=self.request.user)
 
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-class UserList(generics.ListAPIView):
+#class UserList and UserDetail refactored into a single class UserViewSet 
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+	"""
+	This viewset automatically provides `list` and `detail` actions.
+	"""
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
-
-class UserDetail(generics.RetrieveAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
-
-class SnippetHighlight(generics.GenericAPIView):
-	queryset = Snippet.objects.all()
-	renderer_classes = (renderers.StaticHTMLRenderer,)
-
-	def get(self, request, *args, **kwargs):
-		snippet = self.get_object()
-		return Response(snippet.highlighted)
-
-
-#function-based views
-@api_view(('GET',))
-def api_root(request, format=None):
-	return Response({
-		'users': reverse('user-list',request=request, format=format),
-		'snippets':reverse('snippet-list', request=request, format=format)
-	})
