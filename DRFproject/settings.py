40589ab4f188664e0823106b71700a83acb93da3
fey101 <faithkangai.g@gmail.com>
2015-05-23 13:13:02 +0300

    add pagination
    add the REST_FRAMEWORK settings in the DRF_project's settings file to enable pagination of the api

--- DRFproject/settings.py
+++ DRFproject/settings.py
@@ -106,3 +106,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#settings specific to a rest_framework project
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
